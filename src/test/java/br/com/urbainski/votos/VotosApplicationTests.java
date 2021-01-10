8e3431f43f7dd6d693aa0b072ecef5686349ad1e
Cristian Urbainski <cristian.urbainski@ciss.com.br>
2021-01-10 15:26:18 -0300

    Importação inicial do projeto

---
+++ src/test/java/br/com/urbainski/votos/VotosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.urbainski.votos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
